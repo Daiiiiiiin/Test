2cdac83f0a0de3be8af47518c71c526de60b0be6
Daiiiiiiin <12210498in@inha.edu>
2024-01-16 20:15:31 +0900

    2024.01.16

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import sys
 sys.dont_write_bytecode =  True
 
 import time
-from operations.measure_p_oxy import MeasurePpcOxy
-from operations.timer import Timer
+import operations
 
 class SharedMemory:
     def __init__(self):
@@ -12,8 +11,8 @@ class SharedMemory:
 
 sharedmemory = SharedMemory()
 
-measurement = MeasurePpcOxy(1, 10, 1, True, sharedmemory)
-timer = Timer(1, 10, 1, True, sharedmemory)
+measurement = operations.MeasurePpcOxy(1, 10, 1, True, sharedmemory)
+timer = operations.Timer(1, 10, 1, True, sharedmemory)
 time.sleep(3)
 
 sharedmemory.is_running = True
